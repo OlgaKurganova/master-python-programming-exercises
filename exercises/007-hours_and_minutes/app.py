e4877dc4836cdb11dc2844c7c0811e84334366a0
Olga Kurganova <157152724+OlgaKurganova@users.noreply.github.com>
2024-08-23 17:28:56 +0000

    horas_minutos

--- exercises/007-hours_and_minutes/app.py
+++ exercises/007-hours_and_minutes/app.py
@@ -1,6 +1,13 @@
 def hours_minutes(seconds):
   # Your code here
-  return None
+  # Calcula el número de horas
+    hours = seconds // 3600
+    
+    # Calcula el número de minutos restantes después de extraer las horas
+    minutes = (seconds % 3600) // 60
+    
+    # Retorna las horas y los minutos como una tupla
+    return hours, minutes
 
 # Invoke the function and pass any integer as its argument
 print(hours_minutes(3900))
