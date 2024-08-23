648a8809cfde112874f595e8a422549040fb9a68
Olga Kurganova <157152724+OlgaKurganova@users.noreply.github.com>
2024-08-23 17:17:39 +0000

    apple_sharing

--- exercises/006-apple_sharing/app.py
+++ exercises/006-apple_sharing/app.py
@@ -1,6 +1,13 @@
+
 def apple_sharing(n,k):
   # Your code here
-  return None
+  # Calculate how many apples each student gets
+    apples_per_student = k // n
+    # Calculate how many apples remain in the basket
+    remaining_apples = k % n
+
+     # Return both results as a tuple
+    return apples_per_student, remaining_apples
  
 
 print(apple_sharing(6,50))
