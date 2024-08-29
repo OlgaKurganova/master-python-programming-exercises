82655e3b9e23c52bb4b2261eb2ed23789254d86f
Olga Kurganova <157152724+OlgaKurganova@users.noreply.github.com>
2024-08-29 12:07:34 +0000

    //_%

--- exercises/009-two_digits/app.py
+++ exercises/009-two_digits/app.py
@@ -1,7 +1,11 @@
 # Complete the function to return the tens digit and the units digit of any interger
 def two_digits(number):
   # Your code here
-  return None
+  # Obtener el dígito de las decenas dividiendo por 10 y obteniendo la parte entera
+    des_digit = number // 10
+    unidades_left=number % 10
+    print(des_digit)
+    return des_digit, unidades_left
    
 
 # Invoke the function with any two digit integer as its argument
