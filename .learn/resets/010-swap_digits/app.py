3c8fc4ff2c843117b341453d1269c84810ecf8e8
Olga Kurganova <157152724+OlgaKurganova@users.noreply.github.com>
2024-08-29 12:18:12 +0000

    swap_digits

--- .learn/resets/010-swap_digits/app.py
+++ .learn/resets/010-swap_digits/app.py
@@ -1,13 +1,7 @@
 # Complete the function to return the swapped digits of a given two-digit integer
 def swap_digits(num):
   # Your code here
-   # Obtener el dígito de las decenas dividiendo por 10 y obteniendo la parte entera
-    des_digit = num // 10
-    unidades_left=num % 10
-    print(des_digit)
-    return  unidades_left, des_digit
-
-
+  return None
    
 # Invoke the function with any two-digit integer as its argument
 print(swap_digits(30))
