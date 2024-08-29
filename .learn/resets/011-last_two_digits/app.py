6de45598c8260f98ac9bc86040aa5603d121cff1
Olga Kurganova <157152724+OlgaKurganova@users.noreply.github.com>
2024-08-29 12:30:07 +0000

    last_two_digits

--- .learn/resets/011-last_two_digits/app.py
+++ .learn/resets/011-last_two_digits/app.py
@@ -1,10 +1,6 @@
 # Complete the function to print the last two digits of an integer greater than 9
 def last_two_digits(num):
-    last_two = num % 100
-    
-    # Imprimir los últimos dos dígitos
-    print(last_two)
-    
-   
+    return None
+
 # Invoke the function with any integer greater than 9
-print(last_two_digits(1234))
+print(last_two_digits())
