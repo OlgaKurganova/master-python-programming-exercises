3abba9ce66d6a0e6f601162472db8b0d27f5cc0c
Olga Kurganova <157152724+OlgaKurganova@users.noreply.github.com>
2024-08-31 06:50:13 +0000

    digits_sum

--- exercises/013-sum_of_digits/app.py
+++ exercises/013-sum_of_digits/app.py
@@ -1,6 +1,10 @@
 # Complete the function "digits_sum" so that it prints the sum of a three-digit number
 def digits_sum(num):
-  return None
+    
+    num = (num // 10)% 10
+
+    return num+num+num
+
 
 
 # Invoke the function with any three-digit number
