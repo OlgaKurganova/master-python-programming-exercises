bf12dc6d39c30a1a79d5d91817f0d447398f278e
Olga Kurganova <157152724+OlgaKurganova@users.noreply.github.com>
2024-08-31 07:22:02 +0000

    math

--- exercises/015-car_route/app.py
+++ exercises/015-car_route/app.py
@@ -1,7 +1,16 @@
 # Complete the function to return the amount of days it will take to cover a route
+import math
+
 def car_route(n,m):
-  return None
+
+  days = math.ceil(m / n)
+
+  # Verificamos si hay una distancia restante que necesita un día adicional
+     #  if M % N != 0:
+        #   days += 1
+
+  return days
 
 
 # Invoke the function with two integers
-print(car_route())
+print(car_route(20,40))
