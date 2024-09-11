4e55579811a185e15c88588dc0640e6d581c06f8
Olga Kurganova <157152724+OlgaKurganova@users.noreply.github.com>
2024-09-11 12:22:31 +0000

    round(math.sqr

--- exercises/020-factorial/app.py
+++ exercises/020-factorial/app.py
@@ -1,1 +1,8 @@
-# Your code here
+def factorial(n):
+    if n == 0 or n == 1:  # El factorial de 0 y 1 es 1
+        return 1
+    else:
+        return n * factorial(n - 1)  # Llamada recursiva
+
+# Ejemplo de uso
+print(factorial(5))  # Salida: 120
