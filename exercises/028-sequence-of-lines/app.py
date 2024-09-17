b9248aa4b77bc078072ca758ebbff0d8c74e6d4b
Olga Kurganova <157152724+OlgaKurganova@users.noreply.github.com>
2024-09-17 10:44:23 +0000

    hola_upper

--- exercises/028-sequence-of-lines/app.py
+++ exercises/028-sequence-of-lines/app.py
@@ -1,1 +1,18 @@
 # Your code here
+def lines(input_string):
+    # Retornar el string en mayúsculas
+    return input_string.upper()
+
+# Ejemplo de uso
+input_string = "Hola, cómo estás?"
+resultado = lines(input_string)
+print(resultado)
+
+
+
+#  hola = 'Hola pythonista'
+#  hola_upper = hola.upper()
+#  print(hola)
+#  Hola pythonista
+#  print(hola_upper)
+#  HOLA PYTHONISTA
