554347980cb217b0de3a9515f7ddfad449ce3557
Olga Kurganova <157152724+OlgaKurganova@users.noreply.github.com>
2024-09-17 09:58:31 +0000

    two_dimensional_list()

--- exercises/026-two-dimensional-array/app.py
+++ exercises/026-two-dimensional-array/app.py
@@ -1,1 +1,11 @@
 # Your code here
+def  two_dimensional_list(x,y):
+    matrix = [[i * j for j in range(y)] for i in range(x)]
+    return matrix
+x = 3  # número de filas
+y = 5  # número de columnas
+
+result = two_dimensional_list(x,y)
+
+for row in result:
+    print(row)
